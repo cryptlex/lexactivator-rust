a5302c811b70dcc971d58105694dc4cca02db3d1
Mufti Azan Farooqi <muf.naza@gmail.com>
2023-09-22 10:59:24 +0530

    ci: add test automation

--- build.rs
+++ build.rs
@@ -87,7 +87,10 @@ fn set_libs_to_link() {
             println!("cargo:rustc-link-lib=dylib=stdc++");
         } else if #[cfg(target_os="windows")] {
             println!("cargo:rustc-link-lib=winhttp");
+            println!("cargo:rustc-link-lib=crypt32");
             println!("cargo:rustc-link-lib=libcurl_MD");
+            println!("cargo:rustc-link-lib=ws2_32");
+            println!("cargo:rustc-link-lib=shell32");
         }
     }
 }
